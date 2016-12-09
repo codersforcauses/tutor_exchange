776df57bfe2b8ab1b3e14d21a39a01fa3ad8658c
PremAghera <prem.aghera@gmail.com>
2016-12-09 14:23:12 +0800

    Added Server
    Added Server

---
+++ server/index.php
@@ -0,0 +1,46 @@
+<html lang="en">
+<head>
+  <title>Github Testing</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="css/bootstrap.min.css">
+  <link rel="stylesheet" href="css/style.css">
+  <script src="js/jquery-3.1.1.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+
+</head>
+<body>
+
+<?php include_once "navbar.php";?>
+
+<div class="jumbotron text-center">
+  <h1>UWA Guild Volunteering</h1> 
+  <p>Nulla vulputate quam ac erat sodales aliquam. </p>
+	<div class="container">
+		<button class="btn btn-default btn-lg">Search for Tutors</button>
+	</div>
+
+</div>
+
+<div class="container-fluid text-center" id="row1">
+<div class="row">
+  <div class="col-sm-4">
+    <h4>Tutoring is pretty good</h4>
+    <h7>Nunc elementum faucibus magna quis convallis. Aenean nec tellus rutrum, viverra arcu eu, blandit nibh. Curabitur ut porttitor mi. Phasellus egestas ex maximus risus lacinia, et varius lectus tincidunt. Etiam nec tortor justo.</h7>
+  </div>
+  <div class="col-sm-4">
+    <h4>Tutoring is pretty good</h4>
+    <h7>Nulla vulputate quam ac erat sodales aliquam. Nam congue viverra odio ut dictum. Mauris ipsum lorem, facilisis sit amet bibendum ac, luctus ac velit. Proin convallis vel est ac ornare. <h7>
+  </div>
+  <div class="col-sm-4">
+    <h4>Tutoring is pretty good</h4>
+      <h7>Nulla vulputate quam ac erat sodales aliquam. Nam congue viverra odio ut dictum. Mauris ipsum lorem, facilisis sit amet bibendum ac, luctus ac velit. Proin convallis vel est ac ornare. Morbi eu tellus at purus vehicula dapibus ac sit amet est. <h7>
+  </div>
+</div>
+</div>
+
+<?php include_once "footer.php";?>
+
+
+</body>
+</html>
