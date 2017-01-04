cd6e9bb2d9920715c38780c6ba49cdc1efd6ba2d
Michael Baxter <20503664@student.uwa.edu.au>
2017-01-04 16:03:53 +0800

    Added login_success page
    The server project included a simple login success page.

--- client/app/app.js
+++ client/app/app.js
@@ -1,25 +1,33 @@
 angular
-    .module('tutorExchange', [
-        'ui.bootstrap',
-        'ngAnimate',
-        'ui.router'
-    ])
-    .config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider, $stateProvider) {
-        $urlRouterProvider.otherwise('/');
+  .module('tutorExchange', [
+    'ui.bootstrap',
+    'ngAnimate',
+    'ui.router',
+  ])
+  .config(['$urlRouterProvider', '$stateProvider',
+    function($urlRouterProvider, $stateProvider) {
+      $urlRouterProvider
+        .otherwise('/');
 
-        $stateProvider
-            .state('home', {
-                url: '/',
-                templateUrl: 'app/templates/home.html',
-            })
+      $stateProvider
+        .state('home', {
+          url: '/',
+          templateUrl: 'app/templates/home.html',
+        })
 
-            .state('login', {
-                url: '/login',
-                templateUrl: 'app/templates/login.html',
-            })
+        .state('login', {
+          url: '/login',
+          templateUrl: 'app/templates/login.html',
+        })
 
-            .state('apply', {
-                url: '/apply',
-                templateUrl: 'app/templates/apply.html',
-            });
-    }]);
+        .state('login_success', {
+          url: '/login_success',
+          templateUrl: 'app/templates/login_success.html',
+        })
+
+        .state('apply', {
+          url: '/apply',
+          templateUrl: 'app/templates/apply.html',
+        });
+    },
+  ]);
