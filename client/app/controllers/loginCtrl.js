80f3f894f9c82793befc4a09411c9fb4b98461bd
Michael Baxter <20503664@student.uwa.edu.au>
2017-01-08 21:07:42 +0800

    Reworked login controller.
    login controller now sends credentials to server for verification.

--- client/app/controllers/loginCtrl.js
+++ client/app/controllers/loginCtrl.js
@@ -1,36 +1,33 @@
-angular
-  .module('tutorExchange')
-  .controller('LoginCtrl', ['$scope', '$http', '$state', 'myData',
-    function($scope, $http, $state, myData) {
+(function(angular) {
 
-      $scope.submit = function() {
+  'use strict';
 
-        if (!$scope.user) {
-          return;
-        }
+  angular
+    .module('tutorExchange')
+    .controller('LoginCtrl', LoginCtrl);
 
-        $http.get('/users?id=' + $scope.user.id)
-          .then(function(response) {
 
-            if (response.data.length === 0) {
-              console.log('User does not exist');
-              return;
-            }
+  LoginCtrl.$inject = ['$scope', '$http', '$window', '$state'];
+  function LoginCtrl($scope, $http, $window, $state) {
 
-            if ($scope.user.password !== response.data[0].password) {
-              console.log('Wrong password');
-              return;
-            }
+    $scope.submit = function(user) {
 
-            $scope.user.name = response.data[0].name;
-            console.log('Logged in as ' + $scope.user.name);
-            myData.set($scope.user);
-            $state.go('login_success');
-
-          });
+      var credentials = {
+        id:         parseInt(user.id),
+        password:   user.password,
       };
 
+      $http.post('/auth/login', {user: credentials})
+        .then(function(response) {
+          if (response.data.success) {
+            $window.sessionStorage.token = response.data.token;
+            console.log('Logged in as ' + user.id);
+            $state.go('login_success');
+          } else {
+            console.log('Log in unsuccessful');
+          }
+        });
+    };
+  }
 
-
-    },
-  ]);
+})(angular);
