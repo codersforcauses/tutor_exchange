76447e74f539797380d77371f6d3b917c0c4efcb
Michael Baxter <20503664@student.uwa.edu.au>
2017-01-09 05:02:51 +0800

    Tidied up
    Use authService.logout() instead of session.destroy() because it also
    removes token.

--- client/app/controllers/login_successCtrl.js
+++ client/app/controllers/login_successCtrl.js
@@ -6,20 +6,21 @@
     .controller('LoginSuccessCtrl', LoginSuccessCtrl);
 
 
-  LoginSuccessCtrl.$inject = ['$scope', 'session', '$state', '$http', '$window'];
-  function LoginSuccessCtrl($scope, session, $state, $http, $window) {
+  LoginSuccessCtrl.$inject = ['$scope', 'session', 'authService', '$state', '$http'];
+  function LoginSuccessCtrl($scope, session, authService, $state, $http) {
     $scope.user = {name: session.getUserName()};
 
     $scope.logout = function() {
 
-      //$http.get('/api/users?id=' + session.getUserId(), {headers: {'authorization': 'Bearer ' + $window.sessionStorage.token}})
+      //authService.logout();
+      $state.go('home');
+
       $http.get('/api/users?id=' + session.getUserId())
         .then(function(response) {
           console.log(response.data[0].name + ' has left the building');
+          authService.logout();
         });
 
-      session.destroy();
-      $state.go('home');
     };
   }
 
