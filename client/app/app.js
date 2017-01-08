6cc193ae214d1e1eb251d8c3fed78f8469d10358
Michael Baxter <20503664@student.uwa.edu.au>
2017-01-08 17:42:13 +0800

    Tidied up apps
    Using the nifty iife.

--- client/app/app.js
+++ client/app/app.js
@@ -8,35 +8,37 @@
       'ngAnimate',
       'ui.router',
     ])
-    .config(['$urlRouterProvider', '$stateProvider',
-      function($urlRouterProvider, $stateProvider) {
-        $urlRouterProvider
-          .otherwise('/');
+    .config(config);
 
-        $stateProvider
-          .state('home', {
-            url: '/',
-            templateUrl: 'app/templates/home.html',
-          })
 
-          .state('login', {
-            url: '/login',
-            templateUrl: 'app/templates/login.html',
-            controller: 'LoginCtrl',
-          })
+  config.$inject(['$urlRouterProvider', '$stateProvider']);
+  function config($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider
+      .otherwise('/');
 
-          .state('login_success', {
-            url: '/login_success',
-            templateUrl: 'app/templates/login_success.html',
-            controller: 'LoginSuccessCtrl',
-          })
+    $stateProvider
+      .state('home', {
+        url: '/',
+        templateUrl: 'app/templates/home.html',
+      })
 
-          .state('apply', {
-            url: '/apply',
-            templateUrl: 'app/templates/apply.html',
-            controller: 'ApplyCtrl',
-          });
-      },
-    ]);
+      .state('login', {
+        url: '/login',
+        templateUrl: 'app/templates/login.html',
+        controller: 'LoginCtrl',
+      })
+
+      .state('login_success', {
+        url: '/login_success',
+        templateUrl: 'app/templates/login_success.html',
+        controller: 'LoginSuccessCtrl',
+      })
+
+      .state('apply', {
+        url: '/apply',
+        templateUrl: 'app/templates/apply.html',
+        controller: 'ApplyCtrl',
+      });
+  }
 
 })(angular);
