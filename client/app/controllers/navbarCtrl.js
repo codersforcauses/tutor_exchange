b5701027cadf27fc94f2588851fc3312951b4900
Liam <liampeeters@gmail.com>
2017-01-10 14:54:03 +0800

    Navbar Controller Updates on Login.
    Navbar can change based on logged in user type.
    
    Small HTML Reformatting.

--- client/app/controllers/navbarCtrl.js
+++ client/app/controllers/navbarCtrl.js
@@ -5,9 +5,16 @@
     .module('tutorExchange')
     .controller('NavbarCtrl', NavbarCtrl);
 
-  NavbarCtrl.$inject = ['$scope'];
-  function NavbarCtrl($scope) {
+  NavbarCtrl.$inject = ['$scope', 'authService', '$rootScope', 'USER_ROLES'];
+  function NavbarCtrl($scope, authService, $rootScope, USER_ROLES) {
     $scope.isNavCollapsed = true;
+    $scope.$on('$stateChangeStart', function(event, next) {
+        if (next.data && next.data.authRoles && authService.isAuthenticated()) {
+          $scope.loggedinuser = next.data.authRoles[0];
+        } else {
+          delete $scope.loggedinuser;
+        }
+      });
   }
 
 })(angular);
