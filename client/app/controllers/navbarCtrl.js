438439dc27378513448ace45351dddc9620406be
Liam <liampeeters@gmail.com>
2017-01-13 18:24:05 +0800

    Post-Meeting Updates
    - User Roles Updated
    - New Navbar items added. Now detects active page.
    - Renamed Mock Server to API folder

--- client/app/controllers/navbarCtrl.js
+++ client/app/controllers/navbarCtrl.js
@@ -5,11 +5,16 @@
     .module('tutorExchange')
     .controller('NavbarCtrl', NavbarCtrl);
 
-  NavbarCtrl.$inject = ['$scope', 'authService', '$rootScope', 'USER_ROLES', 'userFunctions'];
-  function NavbarCtrl($scope, authService, $rootScope, USER_ROLES, userFunctions) {
+  NavbarCtrl.$inject = ['$scope', '$location', 'authService', '$rootScope', 'USER_ROLES', 'userFunctions'];
+  function NavbarCtrl($scope, $location, authService, $rootScope, USER_ROLES, userFunctions) {
+    $scope.isSelected = function(currentLocation) {
+      return currentLocation === $location.path();
+    };
+
     $scope.isNavCollapsed = true;
     $scope.logout = function() {
       userFunctions.logoutUser();
+      $scope.isLoggedIn = false;
     };
     $scope.$on('$stateChangeStart', function(event, next) {
         if (authService.isAuthenticated()) {
