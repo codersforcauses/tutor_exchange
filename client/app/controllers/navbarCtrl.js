2f8074bf1c4df7d0da919a6bf36099db63e15d79
Liam <liampeeters@gmail.com>
2017-01-13 21:21:02 +0800

    Profile now shows Verification/Visiblity
    Also:
    - Added new Apply Fields
    - Fixed Logout Bug
    - Assorted HTML and CSS changes

--- client/app/controllers/navbarCtrl.js
+++ client/app/controllers/navbarCtrl.js
@@ -7,22 +7,19 @@
 
   NavbarCtrl.$inject = ['$scope', '$location', 'authService', '$rootScope', 'USER_ROLES', 'userFunctions'];
   function NavbarCtrl($scope, $location, authService, $rootScope, USER_ROLES, userFunctions) {
+    $scope.$on('$stateChangeStart', function(event, next) {
+        $scope.isLoggedIn = authService.isAuthenticated();
+      });
+
     $scope.isSelected = function(currentLocation) {
       return currentLocation === $location.path();
     };
 
     $scope.isNavCollapsed = true;
     $scope.logout = function() {
-      userFunctions.logoutUser();
       $scope.isLoggedIn = false;
+      userFunctions.logoutUser();
     };
-    $scope.$on('$stateChangeStart', function(event, next) {
-        if (authService.isAuthenticated()) {
-          $scope.isLoggedIn = true;
-        } else {
-          $scope.isLoggedIn = false;
-        }
-      });
   }
 
 })(angular);
