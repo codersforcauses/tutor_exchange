960ca145aa6b1e2947fc1143c2769cfe037a2687
Liam <liampeeters@gmail.com>
2017-01-20 16:28:44 +0800

    Profile can be Updated
    - Can now update profile fields or set profile to public/hidden. Runs
    off the /api/updateprofile command through a basic JSON-server backend.
    - Fixed Suggestions for Language and Unit. Now Requires minimum
    character input rather than showing all.
    - Assorted Fixes including better error handling for getUserDetails and
    others.

--- client/app/controllers/profileCtrl.js
+++ client/app/controllers/profileCtrl.js
@@ -6,15 +6,58 @@ angular
   .controller('ProfileCtrl', ProfileCtrl);
 
 
-ProfileCtrl.$inject = ['$scope', 'session', 'authService', 'userFunctions', '$state', '$http', 'UWA_UNITS'];
-function ProfileCtrl($scope, session, authService, userFunctions, $state, $http, UWA_UNITS) {
+ProfileCtrl.$inject = ['$scope', 'session', 'authService', 'userFunctions', '$state', '$http', 'UWA_UNITS', 'TUTOR_LANGUAGES'];
+function ProfileCtrl($scope, session, authService, userFunctions, $state, $http, UWA_UNITS, TUTOR_LANGUAGES) {
   $scope.availableUnits = UWA_UNITS;
+  $scope.tutorLanguages = TUTOR_LANGUAGES;
+  $scope.editMode = false;
+
   userFunctions
   .getDetails(session.getUserId())
   .then(function(response) {
-      $scope.user = response.data[0];
-      console.log(response.data[0]);
+      if (response.data.length == 1) {
+        $scope.user = response.data[0];
+        console.log(response);
+      } else {
+        console.log('Unable to Load User Data');
+      }
     }
   );
+
+  $scope.edit = function() {
+    if ($scope.user) {
+      $scope.edituser = angular.copy($scope.user);
+      $scope.editMode = true;
+    }
+  };
+
+  $scope.cancel = function() {
+    if ($scope.edituser) delete $scope.edituser;
+    $scope.editMode = false;
+  };
+
+  $scope.save = function() {
+    if ($scope.user && $scope.edituser) {
+      $scope.user = angular.copy($scope.edituser); /*Direct copy for now, in future may compare values*/
+      userFunctions.updateDetails($scope.user)
+      .then(function(response) {
+        if (response.data.success) {
+          $scope.editMode = false;
+          console.log(response);
+        } else {
+          console.log('Unable to Update User Data');
+        }
+      });
+    }
+  };
+
+  $scope.toggleVisiblity = function() {
+    if ($scope.edituser && $scope.user) {
+      var currentVisiblity = !$scope.edituser.visible;
+      $scope.edituser.visible = currentVisiblity;
+      $scope.user.visible = currentVisiblity;
+    }
+  };
+
 }
 })(angular);
