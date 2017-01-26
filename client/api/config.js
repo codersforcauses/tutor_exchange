6808e3383cd1602e26b8cbeebd8e9717cf07cd9b
Liam <liampeeters@gmail.com>
2017-01-26 11:53:13 +0800

    Server Fixes

--- client/api/config.js
+++ client/api/config.js
@@ -1,3 +1,15 @@
 module.exports = {
   secret: 'imasecretdonttellanyone',
+
+  mysqlSettings: {
+    host: '127.0.0.1',
+    port: '3306',
+    user: 'root',
+    password: 'root',
+    database: 'tutorExchangeDB',
+  },
+
+  server: {
+    port: '8080',
+  },
 };
