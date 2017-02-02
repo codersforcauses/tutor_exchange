fc11c81f105e5281a0786afb065de87964f4620d
BuildTools <unconfigured@null.spigotmc.org>
2017-02-03 02:04:15 +0800

    Added password hashing
    -Passwords are now hashed on application and the hash and salt used are
    stored into the database
    -Login now checks against the hashed password rather than the actual
    password
    -Actual passwords left in for now as they will be impossible to retrieve
    from the hashes alone
    -Will require tweaking to the interactions with the database as I think
    my schema is a little different, which is what was causing problems
    previously

--- client/api/server.js
+++ client/api/server.js
@@ -1,3 +1,4 @@
+'use strict';
 var express     = require('express');
 var mysql       = require('mysql');
 var router      = express.Router();
@@ -6,6 +7,8 @@ var path        = require('path');
 var bodyParser  = require('body-parser');
 
 var config      = require(__dirname + '/config');
+
+var crypto = require('crypto');
 /*var jsonServer  = require('json-server');*/
 /*var jwt         = require('jsonwebtoken');
 
@@ -18,6 +21,41 @@ var app = express();
 /*var server = jsonServer.create();
 server.use(jsonServer.bodyParser);
 server.set('secret', config.secret);*/
+
+/** [from https://code.ciphertrick.com/2016/01/18/salt-hash-passwords-using-nodejs-crypto/]
+ * generates random string of characters i.e salt
+ * @function
+ * @param {number} length - Length of the random string.
+ */
+var genRandomString = function(length){
+    return crypto.randomBytes(Math.ceil(length/2))
+            .toString('hex') /** convert to hexadecimal format */
+            .slice(0,length);   /** return required number of characters */
+};
+/**
+ * hash password with sha512.
+ * @function
+ * @param {string} password - List of required fields.
+ * @param {string} salt - Data to be validated.
+ */
+var sha512 = function(password, salt){
+    var hash = crypto.createHmac('sha512', salt); /** Hashing algorithm sha512 */
+    hash.update(password);
+    var value = hash.digest('hex');
+    return {
+        salt:salt,
+        passwordHash:value
+    };
+};
+function saltHashPassword(userpassword) {
+    var salt = genRandomString(16); /** Gives us salt of length 16 */
+    console.log(typeof salt);
+    var passwordData = sha512(userpassword, salt);
+    /*console.log('UserPassword = '+userpassword);
+    console.log('Passwordhash = '+passwordData.passwordHash);
+    console.log('\nSalt = '+passwordData.salt);*/
+    return passwordData;
+};
 
 router.use(function(req,res,next) {
   console.log('/' + req.method);
@@ -46,20 +84,26 @@ connection.connect (function(error) {
 
 
 app.use('/auth/register', function(req, res) {
-
-  var post  = {
+  var passhashsalt = saltHashPassword(req.body.user.password);
+  var post  = { //this whole section may require tweaking based on your personal db setup
     studentNumber: req.body.user.id,
-    sex: req.body.user.sex,
+    studentEmail: req.body.user.id.toString() + '@student.uwa.edu.au',
     name: req.body.user.name,
+    //lastName: 'irrelephant',
     DOB: req.body.user.DOB,
-    phone: req.body.user.phone,
+    sex: req.body.user.sex,
+    phone: req.body.user.phone, //currently doesn't work
     password: req.body.user.password,
+    passwordHash: passhashsalt.passwordHash,
+    salt: passhashsalt.salt,
+    //accountType: 0,
   };
+
   connection.query('SELECT * FROM user WHERE studentNumber = ?', post.studentNumber, function(err, rows, fields) {
     if (err) {
       res.send(err);
     } else if (rows.length !== 0) {
-      res.json({success: false, message: 'User already Exists'});
+          res.json({success: false, message: 'User already Exists'});
     } else {
       connection.query('INSERT INTO user SET ?', post, function(err, rows, fields) {
         if (err) {
@@ -74,24 +118,37 @@ app.use('/auth/register', function(req, res) {
 
 });
 
-
 app.use('/auth/login', function(req, res) {
+  var userSalt;
+  var userHash;
   var details = {
     studentNumber: req.body.user.id,
     password: req.body.user.password,
   };
-  var query = connection.query('SELECT COUNT(*) AS count FROM user WHERE studentNumber = ? and password = ?', [details.studentNumber, details.password], function(err, rows, fields) {
+  //check if user exists to obtain salt
+  connection.query('SELECT * FROM user WHERE studentNumber = ?', details.studentNumber, function(err, rows, fields)  {
     if (err) {
-      res.json({success: false, message: err});
-    } else if (rows[0].count === 1) {
-      res.json({success: true, message: 'Login was Successful'});
+      res.send(err);
+      return;
+    } else if (rows[0].studentNumber !== details.studentNumber) {
+      res.json({success: false, message: 'idk what goes here'}); //help pls i don't know what I'm doing
     } else {
-      res.json({success: false, message: 'Username or Password was Incorrect'});
-    }
-    return;
+        //get salt and hash from db
+        userSalt = rows[0].salt;
+        userHash = rows[0].passwordHash;
+        var inputHashData = sha512(details.password, userSalt); //PROBLEM HERE
+
+        var query = connection.query('SELECT COUNT(*) AS count FROM user WHERE studentNumber = ? and passwordHash = ?', [details.studentNumber, inputHashData.passwordHash], function(err, rows, fields) {
+          if (err) {
+            res.send(err);
+          } else if (rows[0].count === 1) {
+            res.json({success: true, message: 'Login was Successful'});
+          }
+          return;
+        });
+    };
   });
 });
-
 
 app.use('/auth/test',function(req,res) {
   connection.query('SELECT name from user where sex = "M"', function(err, rows, fields) {
