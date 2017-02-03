13f5d24c49713726c9f61cfa3923f008a0ef7687
FrederickSA <21960842@student.uwa.edu.au>
2017-02-03 12:42:36 +0800

    Uncomment

--- client/app/app.js
+++ client/app/app.js
@@ -55,10 +55,10 @@
         templateUrl: 'templates/apply.html',
         controller: 'ApplyCtrl',
       });
-      /*.state('about', {
+      .state('about', {
         url: '/about',
         templateUrl: 'templates/about.html',
-      });*/
+      });
   }
 
   run.$inject = ['$rootScope', 'authService', 'USER_ROLES', '$state'];
