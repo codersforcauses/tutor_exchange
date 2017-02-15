dab5c9a3c9d0fdd55d988af43d7347b9ac274da7
Michael Baxter <20503664@student.uwa.edu.au>
2017-02-15 19:09:49 +0800

    run function in app.js doesn't require USER_ROLES

--- client/app/app.js
+++ client/app/app.js
@@ -72,8 +72,8 @@
       });
   }
 
-  run.$inject = ['$rootScope', 'authService', 'USER_ROLES', '$state'];
-  function run($rootScope, authService, USER_ROLES, $state) {
+  run.$inject = ['$rootScope', 'authService', '$state'];
+  function run($rootScope, authService, $state) {
     $rootScope.$on('$stateChangeStart', function(event, next) {
       if (next.data && next.data.authRequired) {
         if (!authService.isAuthenticated()) {
