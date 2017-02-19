f9c77e4f4c7369d13f316ed81f36009dab2fe7d7
Michael Baxter <20503664@student.uwa.edu.au>
2017-02-19 22:22:32 +0800

    Added elements to session pages
    Pages now have text, tables, input boxes etc.

--- app/controllers/newSessionCtrl.js
+++ app/controllers/newSessionCtrl.js
@@ -6,8 +6,29 @@
     .controller('NewSessionCtrl', NewSessionCtrl);
 
 
-  NewSessionCtrl.$inject = ['$scope'];
-  function NewSessionCtrl($scope) {
+  NewSessionCtrl.$inject = ['$scope', 'fetchService'];
+  function NewSessionCtrl($scope, fetchService) {
+
+    // Set up date picker
+    $scope.datepicker = {opened: false};
+
+    $scope.datepicker.options = {
+      formatYear: 'yy',
+      maxDate: new Date(2020, 5, 22), // Make this on year in future, or even last day of year.
+      minDate: new Date(),
+      startingDay: 1,
+    };
+
+    $scope.open = function() {
+      $scope.datepicker.opened = true;
+    };
+
+    //Set up unit picker
+    fetchService
+      .fetchUnits()
+      .then(function(response) {
+        if (response.data) $scope.availableUnits = response.data;
+      });
   }
 
 })(angular);
