d03ef8229410df79c3ddd02a329bfef3eb844f46
PremAghera <prem.aghera@gmail.com>
2017-02-22 23:24:26 +0800

    Cant search for yourself, unverified and invisible users
    Can we get some bug testing on this one? I think my db has some 'old db'
    users glitch but when I try it with any entries from recent times it
    works fine.

--- api/server.js
+++ api/server.js
@@ -367,6 +367,7 @@ app.use('/api/data/languages',function(req,res) {
 
 // Search for tutors
 app.use('/api/search', function(req, res) {
+      var user = getUser(req);
 
   if (!req.body || !req.body.query || !req.body.query.units) {
     res.json({success: false, message: 'Invalid Search Query'});
@@ -376,11 +377,11 @@ app.use('/api/search', function(req, res) {
   var searchQuery;
   var resultQuery = [];
 
-  var queryString = 'SELECT GROUP_CONCAT(DISTINCT languageName) AS language, GROUP_CONCAT(DISTINCT unitID) AS unitID, tutor.userID, name, phone, bio FROM tutor JOIN languageTutored ON tutor.userID = languageTutored.tutor JOIN language ON languageTutored.language = language.languageCode JOIN unitTutored ON unitTutored.tutor = tutor.userID JOIN unit ON unitTutored.unit = unit.unitID JOIN user ON user.userID = tutor.userID GROUP BY tutor.userID HAVING unitID LIKE ? AND language LIKE ?';
+  var queryString = 'SELECT GROUP_CONCAT(DISTINCT languageName) AS language, GROUP_CONCAT(DISTINCT unitID) AS unitID, tutor.userID, name, phone, bio FROM tutor JOIN languageTutored ON tutor.userID = languageTutored.tutor JOIN language ON languageTutored.language = language.languageCode JOIN unitTutored ON unitTutored.tutor = tutor.userID JOIN unit ON unitTutored.unit = unit.unitID JOIN user ON user.userID = tutor.userID WHERE visible = 1 AND verified = 1 AND tutor.userID != ? GROUP BY tutor.userID HAVING unitID LIKE ? AND language LIKE ?';
   if (!req.body.query.languages) {
-    searchQuery = mysql.format(queryString, ['%'+req.body.query.units.unitID+'%','%']);
+    searchQuery = mysql.format(queryString, [user.id,'%'+req.body.query.units.unitID+'%','%']);
   } else {
-    searchQuery = mysql.format(queryString, ['%'+req.body.query.units.unitID+'%','%'+req.body.query.languages.languageName+'%']);
+    searchQuery = mysql.format(queryString, [user.id,'%'+req.body.query.units.unitID+'%','%'+req.body.query.languages.languageName+'%']);
   }
 
   connection.query(searchQuery, function(err, rows, fields) {
