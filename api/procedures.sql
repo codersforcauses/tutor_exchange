13082c3b367ede39aacf8c7d1052f0c792c1a68d
Liam <liampeeters@gmail.com>
2017-04-19 14:27:46 +0800

    Added Procedure Statements

---
+++ api/procedures.sql
@@ -0,0 +1,37 @@
+use tutorexchange;
+
+DROP PROCEDURE IF EXISTS getUser;
+
+DELIMITER $
+CREATE PROCEDURE `getUser` (ID INT(8))
+BEGIN
+	SELECT * FROM user WHERE userID = ID;
+END $
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS createUser;
+
+DELIMITER $
+CREATE PROCEDURE `createUser` (ID INT(8), first VARCHAR(100), last VARCHAR(100), dateOfBirth DATE, userSex CHAR(1), phoneNumber VARCHAR(20), pwHash VARCHAR(255), passwordSalt VARCHAR(255))
+BEGIN
+	INSERT INTO user SET userID = ID, firstName = first, lastName = last, DOB = dateOfBirth, sex = userSex, phone = phoneNumber, passwordHash = pwHash;
+END $
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS userExists;
+
+DELIMITER $
+CREATE PROCEDURE `userExists` (ID INT(8))
+BEGIN
+	SELECT COUNT(*) FROM user WHERE userID = ID;
+END $
+DELIMITER ;
+
+DROP PROCEDURE IF EXISTS userIsBanned;
+
+DELIMITER $
+CREATE PROCEDURE `userIsBanned` (ID INT(8))
+BEGIN
+	SELECT COUNT(*) FROM bannedUser WHERE userID = ID;
+END $
+DELIMITER ;
