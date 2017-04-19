f2b25f864407a88671fd08d86843f1dcba364eaa
Liam <liampeeters@gmail.com>
2017-04-19 14:31:27 +0800

    Procedure Fix

--- db/procedures.sql
+++ db/procedures.sql
@@ -12,9 +12,9 @@ DELIMITER ;
 DROP PROCEDURE IF EXISTS createUser;
 
 DELIMITER $
-CREATE PROCEDURE `createUser` (ID INT(8), first VARCHAR(100), last VARCHAR(100), dateOfBirth DATE, userSex CHAR(1), phoneNumber VARCHAR(20), pwHash VARCHAR(255), passwordSalt VARCHAR(255))
+CREATE PROCEDURE `createUser` (ID INT(8), first VARCHAR(100), last VARCHAR(100), dateOfBirth DATE, userSex CHAR(1), phoneNumber VARCHAR(20), pwHash VARCHAR(255), pwSalt VARCHAR(255))
 BEGIN
-	INSERT INTO user SET userID = ID, firstName = first, lastName = last, DOB = dateOfBirth, sex = userSex, phone = phoneNumber, passwordHash = pwHash;
+	INSERT INTO user SET userID = ID, firstName = first, lastName = last, DOB = dateOfBirth, sex = userSex, phone = phoneNumber, passwordHash = pwHash, passwordSalt = pwSalt;
 END $
 DELIMITER ;
 
